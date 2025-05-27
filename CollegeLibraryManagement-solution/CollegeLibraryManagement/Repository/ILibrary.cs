6125f664fcfdcd5404de1ba7800879227f52b1c1
Nadiya-badusha <nadiyabadusha129@gmail.com>
2025-05-27 14:37:27 +0530

    Add files via upload

---
+++ CollegeLibraryManagement-solution/CollegeLibraryManagement/Repository/ILibrary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeLibraryManagement.Repository
+{
+    public interface ILibrary
+    {
+        //abstract methods
+        public void ListAllBooks();
+        public void AddBook();
+        public void EditBook();
+        public void RemoveBook();
+        public void SearchByAuthor();
+        public void SearchByTitle();
+        public void SearchByISBN();
+    }
+}
